fe804b8dd7edd01b4537ba6a0e9892b2ebc3dc73
sumit <thesinghsumit@gmail.com>
2020-02-05 21:35:14 +0530

    Adding advance results middleware

--- routes/courses.js
+++ routes/courses.js
@@ -9,9 +9,12 @@ const {
     deleteCourse
 } = require('../controllers/courses');
 
+const Course = require('../models/Course');
+const advancedResults = require('../middleware/advancedResults');
+
 router
     .route('/')
-    .get(getCourses)
+    .get(advancedResults(Course, { path: 'bootcamp', select: 'name description' }), getCourses)
     .post(addCourse);
 
 router
