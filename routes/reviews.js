2499765a89ce667dd6f4ffea975bed2144180745
sumit singh <sumitsingh.lko95@gmail.com>
2020-07-31 17:40:53 +0530

    Get single review and update seeder

--- routes/reviews.js
+++ routes/reviews.js
@@ -3,7 +3,8 @@ const router = express.Router({ mergeParams: true });
 const { protect, authorize } = require('../middleware/auth');
 
 const {
-    getReviews
+    getReviews,
+    getReview
 } = require('../controllers/reviews');
 
 const Review = require('../models/Review');
@@ -12,5 +13,9 @@ const advancedResults = require('../middleware/advancedResults');
 router
     .route('/')
     .get(advancedResults(Review, { path: 'bootcamp', select: 'name description' }), getReviews)
+
+router
+    .route('/:id')
+    .get(getReview)
 
 module.exports = router;
