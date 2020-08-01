57ed89943ae38ab1ef8ed95872e4ef5f1e5ae692
sumit singh <sumitsingh.lko95@gmail.com>
2020-08-01 23:56:48 +0530

    Logout

--- controllers/auth.js
+++ controllers/auth.js
@@ -64,6 +64,23 @@ exports.login = asyncHandler(async (req, res, next) => {
     sendTokenResponse(user, 200, res);
 });
 
+/**
+ * @description  Log User out / Claer Cookie
+ * @route        GET /api/v1/auth/logout
+ * @access       Private
+ */
+exports.logout = asyncHandler(async (req, res, next) => {
+    res.cookie('token', 'none', {
+        expires: new Date(Date.now() + 10 * 1000),
+        httpOnly: true
+    });
+    
+    res.status(200).json({
+        success: true,
+        data: {}
+    })
+})
+
 
 /**
  * @description  Get Current Logged in User
--- middleware/auth.js
+++ middleware/auth.js
@@ -12,10 +12,12 @@ exports.protect = asyncHandler(async (req, res, next) => {
         req.headers.authorization && 
         req.headers.authorization.startsWith('Bearer')
     ) {
+        // Set token from Bearer token in header
         token = req.headers.authorization.split(' ')[1];
     }
 
     // else if(req.cookies.token) {
+    //     // Set token from Cookie
     //     token = req.cookies.token
     // }
 
